c1d378f65b5df82e71b03ece91e0c03c1e13d979
Antônio Evaldo <antonio.evaldo349@gmail.com>
2023-11-01 10:36:32 -0300

    Vídeo: Usando NPM para instalar o Axios

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ const containerVideos = document.querySelector(".videos__container");
 
 async function buscarEMostrarVideos() {
   try {
-    const busca = await fetch("http://localhost:3000/videos");
-    const videos = await busca.json();
+    const busca = await axios.get("http://localhost:3000/videos");
+
+    const videos = busca.data;
 
     videos.forEach((video) => {
       if (video.categoria == "") {
